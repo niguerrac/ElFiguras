dc4de7769e68c8f259de0b071e48a378cd1f2f6b
Nilson <nguerra@qualitatcorp.cl>
2021-08-02 00:36:35 -0400

    Agregar archivos de proyecto.

---
+++ ElFiguras/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace ElFiguras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCuadrado = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnIzquierda = new System.Windows.Forms.Button();
+            this.btnDerecha = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnBajar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label_x = new System.Windows.Forms.Label();
+            this.label_y = new System.Windows.Forms.Label();
+            this.btnHexagono = new System.Windows.Forms.Button();
+            this.btnCirculo = new System.Windows.Forms.Button();
+            this.btnTriangulo = new System.Windows.Forms.Button();
+            this.btnCircular = new System.Windows.Forms.Button();
+            this.btnMovCuadrado = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCuadrado
+            // 
+            this.btnCuadrado.Location = new System.Drawing.Point(55, 21);
+            this.btnCuadrado.Name = "btnCuadrado";
+            this.btnCuadrado.Size = new System.Drawing.Size(75, 23);
+            this.btnCuadrado.TabIndex = 0;
+            this.btnCuadrado.Text = "Cuadrado";
+            this.btnCuadrado.UseVisualStyleBackColor = true;
+            this.btnCuadrado.Click += new System.EventHandler(this.btnCuadrado_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(55, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 257);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnIzquierda
+            // 
+            this.btnIzquierda.Location = new System.Drawing.Point(88, 447);
+            this.btnIzquierda.Name = "btnIzquierda";
+            this.btnIzquierda.Size = new System.Drawing.Size(75, 23);
+            this.btnIzquierda.TabIndex = 2;
+            this.btnIzquierda.Text = "Izquierda";
+            this.btnIzquierda.UseVisualStyleBackColor = true;
+            this.btnIzquierda.Click += new System.EventHandler(this.btnIzquierda_Click);
+            // 
+            // btnDerecha
+            // 
+            this.btnDerecha.Location = new System.Drawing.Point(234, 447);
+            this.btnDerecha.Name = "btnDerecha";
+            this.btnDerecha.Size = new System.Drawing.Size(75, 23);
+            this.btnDerecha.TabIndex = 3;
+            this.btnDerecha.Text = "Derecha";
+            this.btnDerecha.UseVisualStyleBackColor = true;
+            this.btnDerecha.Click += new System.EventHandler(this.btnDerecha_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(169, 418);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(75, 23);
+            this.btnUp.TabIndex = 4;
+            this.btnUp.Text = "Subir";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnSubir_Click);
+            // 
+            // btnBajar
+            // 
+            this.btnBajar.Location = new System.Drawing.Point(169, 476);
+            this.btnBajar.Name = "btnBajar";
+            this.btnBajar.Size = new System.Drawing.Size(75, 23);
+            this.btnBajar.TabIndex = 5;
+            this.btnBajar.Text = "Bajar";
+            this.btnBajar.UseVisualStyleBackColor = true;
+            this.btnBajar.Click += new System.EventHandler(this.btnBajar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 365);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Multiplicador de movimientos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(234, 363);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label_x
+            // 
+            this.label_x.AutoSize = true;
+            this.label_x.Location = new System.Drawing.Point(395, 363);
+            this.label_x.Name = "label_x";
+            this.label_x.Size = new System.Drawing.Size(12, 13);
+            this.label_x.TabIndex = 9;
+            this.label_x.Text = "x";
+            // 
+            // label_y
+            // 
+            this.label_y.AutoSize = true;
+            this.label_y.Location = new System.Drawing.Point(435, 363);
+            this.label_y.Name = "label_y";
+            this.label_y.Size = new System.Drawing.Size(12, 13);
+            this.label_y.TabIndex = 10;
+            this.label_y.Text = "y";
+            // 
+            // btnHexagono
+            // 
+            this.btnHexagono.Location = new System.Drawing.Point(201, 21);
+            this.btnHexagono.Name = "btnHexagono";
+            this.btnHexagono.Size = new System.Drawing.Size(75, 23);
+            this.btnHexagono.TabIndex = 11;
+            this.btnHexagono.Text = "Hexagono";
+            this.btnHexagono.UseVisualStyleBackColor = true;
+            this.btnHexagono.Click += new System.EventHandler(this.btnHexagono_Click);
+            // 
+            // btnCirculo
+            // 
+            this.btnCirculo.Location = new System.Drawing.Point(372, 21);
+            this.btnCirculo.Name = "btnCirculo";
+            this.btnCirculo.Size = new System.Drawing.Size(75, 23);
+            this.btnCirculo.TabIndex = 12;
+            this.btnCirculo.Text = "Circulo";
+            this.btnCirculo.UseVisualStyleBackColor = true;
+            this.btnCirculo.Click += new System.EventHandler(this.btnCirculo_Click);
+            // 
+            // btnTriangulo
+            // 
+            this.btnTriangulo.Location = new System.Drawing.Point(525, 21);
+            this.btnTriangulo.Name = "btnTriangulo";
+            this.btnTriangulo.Size = new System.Drawing.Size(75, 23);
+            this.btnTriangulo.TabIndex = 13;
+            this.btnTriangulo.Text = "Triangulo";
+            this.btnTriangulo.UseVisualStyleBackColor = true;
+            this.btnTriangulo.Click += new System.EventHandler(this.btnTriangulo_Click);
+            // 
+            // btnCircular
+            // 
+            this.btnCircular.Location = new System.Drawing.Point(458, 417);
+            this.btnCircular.Name = "btnCircular";
+            this.btnCircular.Size = new System.Drawing.Size(114, 40);
+            this.btnCircular.TabIndex = 14;
+            this.btnCircular.Text = "Mov. Circular";
+            this.btnCircular.UseVisualStyleBackColor = true;
+            this.btnCircular.Click += new System.EventHandler(this.btnCircular_Click);
+            // 
+            // btnMovCuadrado
+            // 
+            this.btnMovCuadrado.Location = new System.Drawing.Point(615, 417);
+            this.btnMovCuadrado.Name = "btnMovCuadrado";
+            this.btnMovCuadrado.Size = new System.Drawing.Size(114, 40);
+            this.btnMovCuadrado.TabIndex = 15;
+            this.btnMovCuadrado.Text = "Mov. Cuadrado";
+            this.btnMovCuadrado.UseVisualStyleBackColor = true;
+            this.btnMovCuadrado.Click += new System.EventHandler(this.btnMovCuadrado_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(458, 485);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(114, 33);
+            this.btnCargar.TabIndex = 16;
+            this.btnCargar.Text = "Cargar Archivo";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 572);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnMovCuadrado);
+            this.Controls.Add(this.btnCircular);
+            this.Controls.Add(this.btnTriangulo);
+            this.Controls.Add(this.btnCirculo);
+            this.Controls.Add(this.btnHexagono);
+            this.Controls.Add(this.label_y);
+            this.Controls.Add(this.label_x);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBajar);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnDerecha);
+            this.Controls.Add(this.btnIzquierda);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCuadrado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCuadrado;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnIzquierda;
+        private System.Windows.Forms.Button btnDerecha;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnBajar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label_x;
+        private System.Windows.Forms.Label label_y;
+        private System.Windows.Forms.Button btnHexagono;
+        private System.Windows.Forms.Button btnCirculo;
+        private System.Windows.Forms.Button btnTriangulo;
+        private System.Windows.Forms.Button btnCircular;
+        private System.Windows.Forms.Button btnMovCuadrado;
+        private System.Windows.Forms.Button btnCargar;
+    }
+}
+
---
+++ ElFiguras/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElFiguras
+{
+    public partial class Form1 : Form
+    {
+        Graphics papel;
+        //Variables figuras
+        enum Figura { Cuadrado, Hexagono, Circulo, Triangulo };
+        Figura figura = Figura.Cuadrado;
+
+        SolidBrush brush = new SolidBrush(Color.Yellow);
+        int x=80;
+        int y=80;
+
+        int y_objetivo = 0;
+        int x_objetivo = 0;
+        float radius = 40;
+
+        //Variables Mov
+        float radioMov = 20;
+        float h=0;
+        float k=0;
+
+        int multpiplicador= 1;
+
+        Timer t = new Timer();
+        Timer tCirculo = new Timer();
+        Timer tCuadrado = new Timer();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            tCuadrado.Interval = 10;
+            tCuadrado.Tick += new EventHandler(tCuadrado_Tick);
+
+            tCirculo.Interval = 10;
+            tCirculo.Tick += new EventHandler(tCirculo_Tick);
+
+            t.Interval = 10;
+            t.Tick += new EventHandler(t_Tick);
+
+            y_objetivo = y;
+            x_objetivo = x;
+
+            papel = pictureBox1.CreateGraphics();
+
+            label_x.Text = "X: " + x.ToString();
+            label_y.Text = "Y: " + y.ToString();
+
+            string[] txtData = {"Dimencion",radius.ToString(),"ColorR",brush.Color.R.ToString(), "ColorG", brush.Color.G.ToString(), "ColorB",brush.Color.B.ToString()
+                    ,"Movimiento_punto_A_X",x.ToString(),"Movimiento_punto_A_Y",y.ToString(),"Movimiento_punto_B_X",x_objetivo.ToString(),"Movimiento_punto_C_Y",y_objetivo.ToString()};
+
+            string docPath =
+          Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            File.WriteAllLines(docPath+@"\Figura.fig", txtData);
+        }
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            multpiplicador = (int)numericUpDown1.Value;
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+        //########################
+        #region Dibujar_figuras
+        public void DibujarForma()
+        {
+            
+            switch (figura){
+                case  Figura.Cuadrado:
+                    papel.FillRectangle(brush, x-(radius), y-(radius), radius*2, radius*2);
+                    break;
+                case Figura.Hexagono:
+                    var shape = new PointF[6];
+
+
+                    for (int a = 0; a < 6; a++)
+                    {
+                        shape[a] = new PointF(
+                            x + radius * (float)Math.Cos(a * 60 * Math.PI / 180f),
+                            y + radius * (float)Math.Sin(a * 60 * Math.PI / 180f));
+                    }
+
+                    papel.FillPolygon(brush, shape);
+                    break;
+                case Figura.Circulo:
+                    papel.FillEllipse(brush, x - radius, y - radius,
+                      radius + radius, radius + radius);
+                    break;
+                case Figura.Triangulo:
+                    var Trian = new PointF[3];
+                    Trian[0] = new PointF(x, y - radius);
+                    Trian[1] = new PointF(x-radius, y + radius);
+                    Trian[2] = new PointF(x + radius, y + radius);
+
+                    papel.FillPolygon(brush, Trian);
+                    break;
+            }
+        }
+        private void btnCuadrado_Click(object sender, EventArgs e)
+        {
+            figura = Figura.Cuadrado;
+            papel.Clear(Color.White);
+            DibujarForma();
+        }
+        private void btnHexagono_Click(object sender, EventArgs e)
+        {
+            figura = Figura.Hexagono;
+            papel.Clear(Color.White);
+            DibujarForma();
+        }
+
+        private void btnCirculo_Click(object sender, EventArgs e)
+        {
+            figura = Figura.Circulo;
+            papel.Clear(Color.White);
+            DibujarForma();
+        }
+
+        private void btnTriangulo_Click(object sender, EventArgs e)
+        {
+            figura = Figura.Triangulo;
+            papel.Clear(Color.White);
+            DibujarForma();
+        }
+        #endregion
+        //########################
+        #region Movimientos
+        private void btnSubir_Click(object sender, EventArgs e)
+        {
+            y_objetivo -= multpiplicador;
+            t.Stop();
+            tCirculo.Stop();
+            tCuadrado.Stop();
+            t.Start();
+        }
+        private void btnIzquierda_Click(object sender, EventArgs e)
+        {
+            x_objetivo -= multpiplicador;
+            t.Stop();
+            tCirculo.Stop();
+            tCuadrado.Stop();
+            t.Start();
+        }
+        private void btnBajar_Click(object sender, EventArgs e)
+        {
+            y_objetivo += multpiplicador;
+            t.Stop();
+            tCirculo.Stop();
+            tCuadrado.Stop();
+            t.Start();
+        }
+
+        private void btnDerecha_Click(object sender, EventArgs e)
+        {
+            x_objetivo += multpiplicador;
+            t.Stop();
+            tCirculo.Stop();
+            tCuadrado.Stop();
+            t.Start();
+        }
+        private void t_Tick(object sender, EventArgs e)
+        {
+
+            //x++;
+            if (y_objetivo < y)
+            {
+                y--;
+            }
+            if (y_objetivo > y)
+            {
+                y++;
+            }
+            if (x_objetivo < x)
+            {
+                x--;
+            }
+            if (x_objetivo > x)
+            {
+                x++;
+            }
+            label_x.Text = "X: " + x.ToString();
+            label_y.Text = "Y: " + y.ToString();
+
+            papel.Clear(Color.White);
+            DibujarForma();
+
+            if (x == x_objetivo && y == y_objetivo)
+                t.Stop();
+        }
+        private void btnCircular_Click(object sender, EventArgs e)
+        {
+            h = x;
+            k = y + radioMov* multpiplicador ;
+
+            CambioSigno = false;
+            tCirculo.Stop();
+            tCuadrado.Stop();
+            t.Stop();
+            tCirculo.Start();
+        }
+        bool CambioSigno = false;
+        private void tCirculo_Tick(object sender, EventArgs e)
+        {
+
+            if (!CambioSigno)
+            {
+                if (x < h + radioMov * multpiplicador)
+                {
+                    x++;
+                    y = (int)(-Math.Sqrt(radioMov * multpiplicador * radioMov * multpiplicador - Math.Pow(x - h, 2)) + k);
+                }
+                else { CambioSigno = true; }
+            }
+
+            if (CambioSigno)
+            {
+                if (x > h - radioMov * multpiplicador)
+                {
+                    x--;
+                    y = (int)(Math.Sqrt(radioMov * multpiplicador * radioMov * multpiplicador - Math.Pow(x - h, 2)) + k);
+                }
+                else { CambioSigno = false; }
+            }
+           
+            label_x.Text = "X: " + x.ToString();
+            label_y.Text = "Y: " + y.ToString();
+            y_objetivo = y;
+            x_objetivo = x;
+
+            papel.Clear(Color.White);
+            DibujarForma();
+            if (x == h && y == k-radioMov* multpiplicador)
+                tCirculo.Stop();
+        }
+
+        private void btnMovCuadrado_Click(object sender, EventArgs e)
+        {
+            h = x;
+            k = y;
+            CambioSigno = false;
+            tCirculo.Stop();
+            tCuadrado.Stop();
+            t.Stop();
+            tCuadrado.Start();
+        }
+        private void tCuadrado_Tick(object sender, EventArgs e)
+        {
+            
+            if (!CambioSigno)
+            {
+                if (x < h +radioMov* multpiplicador  * 2)
+                    x++;
+                else
+                {
+                    if (y < k +radioMov* multpiplicador * 2)
+                        y++;
+                    else
+                        CambioSigno = true;
+                }
+            }
+            if (CambioSigno)
+            {
+                if (x > h)
+                    x--;
+                else
+                {
+                    if (y > k)
+                        y--;
+                    else
+                        CambioSigno = false;
+                }
+            }
+
+            label_x.Text = "X: " + x.ToString();
+            label_y.Text = "Y: " + y.ToString();
+            y_objetivo = y;
+            x_objetivo = x;
+            papel.Clear(Color.White);
+            DibujarForma();
+            if (x == h && y == k)
+                tCuadrado.Stop();
+        }
+        #endregion
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            t.Stop();
+            tCirculo.Stop();
+            tCuadrado.Stop();
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Seleccion Figura";
+            string docPath =
+          Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog1.InitialDirectory = docPath;
+            openFileDialog1.Filter = "All files (*.fig*)|*.fig*|All files (*.fig*)|*.fig*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string[] text = File.ReadAllLines(file);
+
+                    if (text.Length == 16)
+                    {
+                        radius = float.Parse(text[1], CultureInfo.InvariantCulture.NumberFormat);
+                        Color color = new Color();
+                        color = Color.FromArgb(Int32.Parse(text[3]),Int32.Parse(text[5]) , Int32.Parse(text[7]));
+                        brush.Color = color;
+                        x = Int32.Parse(text[9]);
+                        y = Int32.Parse(text[11]);
+                        x_objetivo = Int32.Parse(text[13]);
+                        y_objetivo = Int32.Parse(text[15]);
+                        papel.Clear(Color.White);
+                        DibujarForma();
+                        
+                        t.Start();
+                    }
+                }
+                catch (IOException)
+                {
+                }
+            }
+
+        }
+    }
+}
